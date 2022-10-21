b4abf7fda6449ac6ba35d7d42566e9a7842c2957
rav <115064706+ravabasya06@users.noreply.github.com>
2022-10-21 15:51:16 +0700

    Create guessr.py

---
+++ guessr.py
@@ -0,0 +1,29 @@
+import random
+def ulang():
+    jar1 = int(input("Masukkan jarak pertama : "))
+    jar2 = int(input("Masukkan jarak kedua : "))
+    chance = int(input("Masukkan berapa kesempatan yang dapat anda lakukan : "))
+    randomN = random.randint(jar1, jar2)
+    
+    while chance >= 0:
+        guessN = int(input("Tebak berapa angka yang dihasilkan : "))
+        if  guessN > randomN:
+            print("Angka yang anda tebak lebih besar dari yang dihasilkan")
+        elif guessN < randomN:
+            print("Angka yang anda tebak lebih kecil dari yang dihasilkan")
+        elif chance == 0:
+            print("Anda kehilangan kesempatan")
+            break
+        else:
+            print("Angka yang anda tebak benar")
+            break
+        
+    pengulangan = input("Lakukan game lagi? ya / tidak : ")
+    
+    if pengulangan == "ya":
+        ulang()
+    elif pengulangan == "tidak":
+        print("Ok")
+    else:
+        print("Saya tidak mengerti perintah anda, saya akan menganggap anda menjawab tidak.")
+ulang()
